399184b439db5faf0ea5d0e52a7d1a7ab0959e48
rjrodger <richard@ricebridge.com>
2019-10-18 12:22:39 +0100

    convert to typescript

--- inks.js
+++ inks.js
@@ -2,82 +2,66 @@
   MIT License,
   Copyright (c) 2016-2018, Richard Rodger and other contributors.
 */
-
-'use strict'
-
-
-module.exports = Inks
-
+'use strict';
+module.exports = Inks;
 function Inks(tm, ctxt) {
-  tm = tm.replace(/\\`/g, '\x07')
-
-  var s = null
-  var m = null
-  var buf = []
-  var last = 0
-  while(m = tm.substring(last).match(/(`.*?`)/)) {
-    var cs = m[0]
-
-    s = tm.substring(last,last+m.index)
-    if(''!==s) { buf.push(s) }
-
-    last = last + m.index + m[0].length
-
-    var csm = null
-    if(csm = cs.match(/^`(.*?):(.*?)`$/)) {
-      var key = csm[1]
-      var path = csm[2]
-      var obj = ctxt[key]
-      var val = null
-
-      if('object' === typeof obj) {
-        buf.push(handle_eval('$obj.'+path, obj, ctxt))
-      }
-      else {
-        buf.push(null)
-      }
+    tm = tm.replace(/\\`/g, '\x07');
+    var s = null;
+    var m = null;
+    var buf = [];
+    var last = 0;
+    while ((m = tm.substring(last).match(/(`.*?`)/))) {
+        var cs = m[0];
+        s = tm.substring(last, last + m.index);
+        if ('' !== s) {
+            buf.push(s);
+        }
+        last = last + m.index + m[0].length;
+        var csm = null;
+        if ((csm = cs.match(/^`(.*?):(.*?)`$/))) {
+            var key = csm[1];
+            var path = csm[2];
+            var obj = ctxt && ctxt[key];
+            var val = null;
+            if ('object' === typeof obj) {
+                buf.push(handle_eval('$obj.' + path, obj, ctxt));
+            }
+            else {
+                buf.push(null);
+            }
+        }
+        else {
+            buf.push(handle_eval(cs.substring(1, cs.length - 1), null, ctxt));
+        }
+    }
+    s = tm.substring(last, tm.length);
+    if ('' !== s) {
+        buf.push(s);
+    }
+    var out = null;
+    // Preserve type if single value
+    if (1 === buf.length) {
+        out = buf[0];
     }
     else {
-      buf.push(handle_eval(cs.substring(1,cs.length-1), null, ctxt))
+        buf = buf.map(function (x) {
+            if ('string' === typeof x || 'number' === typeof x) {
+                return x;
+            }
+            else
+                return JSON.stringify(x);
+        });
+        out = buf.join('');
     }
-  }
-  s = tm.substring(last,tm.length)
-  if(''!==s) { buf.push(s) }
-
-  //console.log('B',buf)
-  
-
-  var out = null
-
-  // Preserve type if single value
-  if( 1 === buf.length ) {
-    out = buf[0]
-  }
-  else {
-    buf = buf.map((x)=>{
-      if('string' === typeof x || 'number' === typeof x) {
-        return x
-      }
-      else return JSON.stringify(x)
-    })
-    out = buf.join('')
-  }
-  
-  if('string' === typeof out) {
-    out = out.replace(/\x07/g, '`')
-  }
-
-  return out
+    if ('string' === typeof out) {
+        out = out.replace(/\x07/g, '`');
+    }
+    return out;
 }
-
-
-function handle_eval() {
-  var $val = null
-  var $vstr = arguments[0]
-  var $obj = arguments[1]
-  var $ = arguments[2]
-
-  eval('$val = '+$vstr)
-
-  return null == $val ? null : $val
+// NOTE: function arguments are used by `eval`!
+function handle_eval($vstr, $obj, $) {
+    var $val = null;
+    eval('$val = ' + $vstr);
+    return null == $val ? null : $val;
 }
+//# sourceMappingURL=inks.js.map
--- test/inks.test.js
+++ test/inks.test.js
@@ -3,8 +3,8 @@
 
 var Inks = require('..')
 
-var Code = require('code')
-var Lab = require('lab')
+var Code = require('@hapi/code')
+var Lab = require('@hapi/lab')
 
 var lab = (exports.lab = Lab.script())
 var describe = lab.describe
@@ -12,17 +12,23 @@ var it = lab.it
 var expect = Code.expect
 
 describe('inks', function() {
-  it('happy', function(fin) {
-    expect(Inks('`foo:a`',{foo:{a:'z'}})).equal('z')
-    expect(Inks('`foo:a`~`foo:b`',{foo:{a:'z',b:'y'}})).equal('z~y')
-    expect(Inks('`foo:a`',{foo:{a:1}})).equal(1)
-    expect(Inks('`foo:a`',{foo:{a:{b:1}}})).equal({b:1})
-    expect(Inks('|`foo:a`|',{foo:{a:{b:1}}})).equal('|{"b":1}|')
-    expect(Inks('`bar:a`',{foo:{a:1}})).equal(null)
-    expect(Inks('`foo:b`',{foo:{a:1}})).equal(null)
-    expect(Inks('`1+1`',{})).equal(2)
-    expect(Inks('`$.a`x`$.b`y`$.c`',{a:'',b:'',c:''})).equal('xy')
-    expect(Inks('q`$.a`x`$.b`y`$.c`w',{a:'',b:'',c:''})).equal('qxyw')
-    fin()
+  it('happy', async () => {
+    expect(Inks('`foo:a`')).equal(null)
+    expect(Inks('`foo:a`', { foo: {} })).equal(null)
+    expect(Inks('`foo:a`', { foo: { a: 'z' } })).equal('z')
+    expect(Inks('`foo:a`', { foo: { a: 1 } })).equal(1)
+    expect(Inks('`foo:a`', { foo: { a: {x:1} } })).equal({x:1})
+    expect(Inks('`foo:a`~`foo:b`', { foo: { a: 'z', b: 'y' } })).equal('z~y')
+    expect(Inks('`foo:a`', { foo: { a: 1 } })).equal(1)
+    expect(Inks('`foo:a`', { foo: { a: { b: 1 } } })).equal({ b: 1 })
+    expect(Inks('|`foo:a`|', { foo: { a: { b: 1 } } })).equal('|{"b":1}|')
+    expect(Inks('|`foo:a`|', { foo: { a: 'x' } })).equal('|x|')
+    expect(Inks('|`foo:a`|', { foo: { a: 9 } })).equal('|9|')
+    expect(Inks('`bar:a`', { foo: { a: 1 } })).equal(null)
+    expect(Inks('`foo:b`', { foo: { a: 1 } })).equal(null)
+    expect(Inks('`1+1`', {})).equal(2)
+    expect(Inks('`1+1`')).equal(2)
+    expect(Inks('`$.a`x`$.b`y`$.c`', { a: '', b: '', c: '' })).equal('xy')
+    expect(Inks('q`$.a`x`$.b`y`$.c`w', { a: '', b: '', c: '' })).equal('qxyw')
   })
 })
